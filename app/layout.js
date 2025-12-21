0b7634828428d2cf7ae372c1d406bb1422d45353
sodetteam2024 <sodetteam2024@gmail.com>
2025-12-21 02:48:04 +0000

    arreglando esa vaina

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Header from "@/app/components/header";
+import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
